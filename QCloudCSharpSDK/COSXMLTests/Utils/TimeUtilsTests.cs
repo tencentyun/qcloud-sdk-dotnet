5df29bdd8edb422d3ed8ecf2b503b3d686ef2904
bradyxiao <bradyxiao@tencent.com>
2019-01-22 18:02:03 +0800

    add Log

--- QCloudCSharpSDK/COSXMLTests/Utils/TimeUtilsTests.cs
+++ QCloudCSharpSDK/COSXMLTests/Utils/TimeUtilsTests.cs
@@ -1,7 +1,33 @@
-﻿using System;
+﻿using NUnit.Framework;
+using COSXML.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
+namespace COSXML.Utils.Tests
+{
+    [TestFixture()]
+    public class TimeUtilsTests
+    {
+        [Test()]
+        public void GetCurrentTimeTest()
+        {
+            long days = TimeUtils.GetCurrentTime(TimeUnit.DAYS);
+            long hours = TimeUtils.GetCurrentTime(TimeUnit.HOURS);
+            long minutes = TimeUtils.GetCurrentTime(TimeUnit.MINUTES);
+            long seconds = TimeUtils.GetCurrentTime(TimeUnit.SECONDS);
+            
+
+        }
+
+        [Test()]
+        public void GetFormatTimeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
 
 
 /* ============================================================================== 
@@ -9,10 +35,3 @@ using System.Text;
 * Auth：bradyxiao 
 * Date：2019/1/22 17:32:45 
 * ==============================================================================*/
-
-namespace COSXMLTests.Utils
-{
-    class TimeUtilsTests
-    {
-    }
-}
