9846cc48691d67a8904aed1d56a861d290d741b4
bradyxiao <bradyxiao@tencent.com>
2019-01-22 17:34:27 +0800

    add ut

---
+++ QCloudCSharpSDK/COSXMLTests/Utils/TimeUtilsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/* ============================================================================== 
+* Copyright 2016-2019 Tencent Cloud. All Rights Reserved.
+* Auth：bradyxiao 
+* Date：2019/1/22 17:32:45 
+* ==============================================================================*/
+
+namespace COSXMLTests.Utils
+{
+    class TimeUtilsTests
+    {
+    }
+}
