ea29bb251bce4d98efc7ea89cdfffcdedc211b91
wjielai <gouki0123@gmail.com>
2020-12-28 10:11:12 +0800

    update demo

--- COSXMLDemo/Program.cs
+++ COSXMLDemo/Program.cs
@@ -10,15 +10,18 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using COSXML.CosException;
+using System.Threading;
+using System.Threading.Tasks;
+using COSXML.Transfer;
 
 namespace COSXMLDemo
 {
     public class Program
     {
         
-        internal static string bucket = @"bucket-4-csharp-demo-1253653367";
+        internal static string bucket = @"bucket-4-dotnet-demo-1253653367";
 
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
 
             // 腾讯云 SecretId
@@ -57,7 +60,11 @@ namespace COSXMLDemo
 
                 // 上传对象
                 Console.WriteLine(" ======= Put Object ======");
-                string cosKey = PutObject(cosXml);
+                string cosKey = await PutObject(cosXml);
+
+                // 下载对象
+                Console.WriteLine(" ======= Get Object ======");
+                await GetObject(cosXml, cosKey);
 
                 // 删除对象
                 Console.WriteLine(" ======= Delete Object ======");
@@ -115,18 +122,68 @@ namespace COSXMLDemo
             Console.WriteLine(result.GetResultInfo());
         }
 
-        internal static string PutObject(CosXmlServer cosXml) 
+        internal static async Task<String> PutObject(CosXmlServer cosXml) 
         {
             string cosKey = "cosKey";
-            byte[] tmpData = new byte[1024];
+            //.cssg-snippet-body-start:[transfer-upload-file]
+            // 初始化 TransferConfig
+            TransferConfig transferConfig = new TransferConfig();
+            
+            // 初始化 TransferManager
+            TransferManager transferManager = new TransferManager(cosXml, transferConfig);
+            
+            String cosPath = cosKey; //对象在存储桶中的位置标识符，即称对象键
+            String srcPath = @"本地绝对路径";//本地文件绝对路径
+            
+            // 上传对象
+            COSXMLUploadTask uploadTask = new COSXMLUploadTask(bucket, cosPath);
+            uploadTask.SetSrcPath(srcPath);
+            
+            uploadTask.progressCallback = delegate (long completed, long total)
+            {
+                Console.WriteLine(String.Format("progress = {0:##.##}%", completed * 100.0 / total));
+            };
 
-            PutObjectRequest request = new PutObjectRequest(bucket, cosKey, tmpData);
-
-            PutObjectResult result = cosXml.PutObject(request);
-
-            Console.WriteLine(result.GetResultInfo());
+            try {
+                COSXML.Transfer.COSXMLUploadTask.UploadTaskResult result = await 
+                    transferManager.UploadAsync(uploadTask);
+                Console.WriteLine(result.GetResultInfo());
+                string eTag = result.eTag;
+            } catch (Exception e) {
+                Console.WriteLine("CosException: " + e);
+            }
 
             return cosKey;
+        }
+
+        internal static async Task GetObject(CosXmlServer cosXml, string cosKey) 
+        {
+            TransferConfig transferConfig = new TransferConfig();
+        
+            // 初始化 TransferManager
+            TransferManager transferManager = new TransferManager(cosXml, transferConfig);
+            
+            String cosPath = cosKey; //对象在存储桶中的位置标识符，即称对象键
+            string localDir = System.IO.Path.GetTempPath();//本地文件夹
+            string localFileName = "my-local-temp-file"; //指定本地保存的文件名
+            
+            // 下载对象
+            COSXMLDownloadTask downloadTask = new COSXMLDownloadTask(bucket, cosPath, 
+            localDir, localFileName);
+            
+            downloadTask.progressCallback = delegate (long completed, long total)
+            {
+                Console.WriteLine(String.Format("progress = {0:##.##}%", completed * 100.0 / total));
+            };
+
+            try {
+                COSXML.Transfer.COSXMLDownloadTask.DownloadTaskResult result = await 
+                    transferManager.DownloadAsync(downloadTask);
+                Console.WriteLine(result.GetResultInfo());
+                string eTag = result.eTag;
+            } catch (Exception e) {
+                Console.WriteLine("CosException: " + e);
+            }
         }
 
         internal static void DeleteObject(CosXmlServer cosXml, string cosKey) 
