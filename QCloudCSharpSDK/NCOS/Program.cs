ea5d98734847cb737885b8316d8ee411432438aa
nedzzhang <nedzzhang@tencent.com>
2023-08-25 17:29:14 +0800

    增加配置

---
+++ QCloudCSharpSDK/NCOS/Program.cs
@@ -0,0 +1,17 @@
+using COSXML.Model.Object;
+using COSXML.Auth;
+using COSXML.Transfer;
+using System;
+using COSXML;
+using System.Threading.Tasks;
+using COSXML.Model.Tag;
+using COSXML.Model.Bucket;
+
+
+namespace Process
+{
+    public class Process
+    {
+
+    }
+}
