2cccefbf332f7c6ec952119075f0a716479a6332
weiyankong <weiyankong@outlook.com>
2024-03-13 14:17:05 +0800

    modify version

--- QCloudCSharpSDK/COSXML/Common/CosVersion.cs
+++ QCloudCSharpSDK/COSXML/Common/CosVersion.cs
@@ -9,7 +9,7 @@ namespace COSXML.Common
 {
     public sealed class CosVersion
     {
-        private static string SDKVersion = "5.4.36.0";
+        private static string SDKVersion = "5.4.37.0";
 
         public static string GetUserAgent()
         {
