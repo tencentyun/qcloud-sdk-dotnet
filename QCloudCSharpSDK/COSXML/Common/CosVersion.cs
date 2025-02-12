ca2fd6192f34fdb1b6c5eb65878e31e06b493059
nedzzhang <nedzzhang@tencent.com>
2025-02-12 07:47:00 +0000

    版本号更新

--- QCloudCSharpSDK/COSXML/Common/CosVersion.cs
+++ QCloudCSharpSDK/COSXML/Common/CosVersion.cs
@@ -9,7 +9,11 @@ namespace COSXML.Common
 {
     public sealed class CosVersion
     {
-        private static string SDKVersion = "5.4.42.0";
+        private static string SDKVersion = "5.4.44.0";
+        // 主版本号：第一个数字，产品改动较大，可能无法向后兼容（要看具体项目）
+        // 子版本号：第二个数字，增加了新功能，向后兼容 
+        // 修正版本号：第三个数字，修复BUG 或优化代码，一般没有添加新功能，向后兼容
+        // 编译版本号：通常是系统自动生成，每次代码提交都会导致自动加1.
 
         public static string GetUserAgent()
         {
